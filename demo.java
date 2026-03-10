78b166a737181799a74314c78f5893edb79667bb
Ansh0305 <saiansh2016@gmail.com>
2026-03-11 01:09:20 +0530

    Smallest and Largest number in a array

--- demo.java
+++ demo.java
@@ -44,8 +44,10 @@ class Solution{
 public class demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[5];
-        for (int i = 0; i < 5; i++) {
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
             if (sc.hasNextInt()) {
                 arr[i] = sc.nextInt(); 
             }

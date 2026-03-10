195bd0e06cce16748d2a29c10abe6e799bc60f3c
Ansh0305 <saiansh2016@gmail.com>
2026-03-11 00:59:37 +0530

    Smallest and Largest number in a array

---
+++ demo.java
@@ -0,0 +1,57 @@
+
+// 1. Smallest Element in the Array
+import java.util.Scanner;
+class Solution{
+    // Brute Force
+    /* 
+    public static int sortArr(int[] arr){
+        // Built in Feature
+        // Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
+
+        // Manual Sorting
+        for(int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        return arr[0];
+
+     }
+        */
+
+     // Optimal Solution
+     public static int SmallestElement(int[] arr){
+        int n = arr.length;
+        int min = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+     }
+    //  TC = O(n)
+
+    }
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt(); 
+            }
+            
+        }
+        System.out.println("Smallest element is: " + Solution.SmallestElement(arr));
+        sc.close();
+    }
+}

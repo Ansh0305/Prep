d8ed907f4407885725c0d0c36155dedd18a3a4e1
Ansh0305 <saiansh2016@gmail.com>
2026-03-15 23:19:39 +0530

    Remove Duplicates in-place from Sorted Array-Brute

---
+++ Removing Duplicates from Sorted Array/RemoveDuplicates.java
@@ -0,0 +1,39 @@
+// Remove Duplicates in-place from Sorted Array
+
+// Brute Force approach
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static void main(String[] args){
+        // input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Logic
+        HashSet<Integer> seen = new HashSet<>();
+        int index = 0;
+
+        for(int num : arr){
+            if (!seen.contains(num)) {
+                seen.add(num);
+                arr[index] = num;
+                index++;
+            }
+        }
+        // Output
+        System.out.println("Given Array: " + Arrays.toString(arr)); 
+
+        System.out.print("After removing duplicates: "); 
+        for (int i = 0; i < index; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}

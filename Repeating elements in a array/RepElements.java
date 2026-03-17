4b0eb12e5943b4d4bf4b27c62ef4b4769f1a14df
Ansh0305 <saiansh2016@gmail.com>
2026-03-17 15:05:57 +0530

    Find all repeating elements in an array - Optimal

--- Repeating elements in a array/RepElements.java
+++ Repeating elements in a array/RepElements.java
@@ -1,6 +1,6 @@
 // Find all repeating elements in an array
 
-/* Brute Force */
+/* Brute Force
 import java.util.*;
 public class RepElements{
     public static void main(String[] args){
@@ -35,3 +35,41 @@ public class RepElements{
 }
 // TC: O(n^2) + O(n)
 // SC: O(n)
+
+*/
+
+// Optimal- HashMap
+
+import java.util.*;
+public class RepElements{
+    public static void main(String[] args) {
+        // Input
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Logic
+        Map<Integer, Integer> elementCount = new HashMap<>();
+
+        // Count the occurence of each element
+        for(int i : arr){
+            elementCount.put(i, elementCount.getOrDefault(i, 0) + 1);
+        }
+
+        // Output
+        System.out.print("The repeated values are: ");
+        // Print the elements that appear more than once
+        for(Map.Entry<Integer, Integer> entry : elementCount.entrySet()){
+            if(entry.getValue() > 1){
+                System.out.print(entry.getKey() + " ");
+            }
+        }
+    }
+}
+
+// TC: O(N)
+// SC: O(N)

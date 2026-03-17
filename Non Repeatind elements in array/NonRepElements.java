91fc48931358998f40c25e0de2cc90ae1fe636e3
Ansh0305 <saiansh2016@gmail.com>
2026-03-17 15:33:26 +0530

    Find all the non-repeating elements in an array- Optimal

--- Non Repeatind elements in array/NonRepElements.java
+++ Non Repeatind elements in array/NonRepElements.java
@@ -1,6 +1,6 @@
 // Find all the non-repeating elements in an array
 
-/*Brute Force */
+/*Brute Force
 
 import java.util.*;
 public class NonRepElements {
@@ -35,3 +35,38 @@ public class NonRepElements {
 
 // TC: O(n^2)
 // TC: O(n)
+
+*/
+
+// Optimal - HashMap
+
+import java.util.*;
+public class NonRepElements{
+    public static void main(String[] args) {
+        // Input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // Logic
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Count occurance of each element
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        System.out.print("Non-repeating elements are: ");
+        for(int num : arr){
+            if(map.get(num) == 1){
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
+
+// TC: O(N)
+// SC: O(N)
